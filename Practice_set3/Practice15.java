ca6671a951193481e013d181ae058531afe94ef0
rep-demo <aashishmijar44@gmail.com>
2025-08-16 23:29:44 +0545

    Practice 15 file added

---
+++ Practice_set3/Practice15.java
@@ -0,0 +1,9 @@
+public class Practice15 {
+    public static void main(String[] args) {
+        // Removing whitespace from string;
+        String name="   Aashish Mijar  ";
+        String cleanedName=name.trim();
+        System.out.println(cleanedName);
+        System.out.println(name);
+    }
+}
