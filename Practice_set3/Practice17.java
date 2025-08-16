dd2350fae7c0faf06f6c08b151d0711dc64cefd3
rep-demo <aashishmijar44@gmail.com>
2025-08-16 23:31:26 +0545

    Practice 17 file added

---
+++ Practice_set3/Practice17.java
@@ -0,0 +1,11 @@
+public class Practice17 {
+    public static void main(String[] args) {
+        String name="";
+        if(name==null || name.isEmpty()){
+            System.out.println("String is empty. No name provided");
+        }
+        else{
+            System.out.println("Provided name is: "+name);
+        }
+    }
+}
