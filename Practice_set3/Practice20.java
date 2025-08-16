acfc50f99cf960a322626a96329aa975e70f10f6
rep-demo <aashishmijar44@gmail.com>
2025-08-16 23:32:33 +0545

    Practice 20 file added

---
+++ Practice_set3/Practice20.java
@@ -0,0 +1,14 @@
+enum Example{
+    // PURPLE, RED, GREEN, BLUE
+
+    SUCCESS("success");
+    FALIURE()
+}
+
+public class Practice20 {
+    public static void main(String[] args) {
+        // String input="BLUE";
+        // Example example=Example.valueOf(input);
+        // System.out.println("The color is: "+example);
+    }
+}
