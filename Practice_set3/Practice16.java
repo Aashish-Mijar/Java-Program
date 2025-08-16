4f220aa40b3f6b0736aeb32c1d77df885cfca1f1
rep-demo <aashishmijar44@gmail.com>
2025-08-16 23:30:11 +0545

    Practice 16 file added

---
+++ Practice_set3/Practice16.java
@@ -0,0 +1,8 @@
+public class Practice16 {
+    public static void main(String[] args) {
+        int arr[]={1,3,4,5,33,56};
+        for(int i:arr){
+            System.out.println(i);
+        }
+    }
+}
